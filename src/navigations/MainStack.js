bb2bb57ebaa82e5929a6fdb9f0276b26c7cb2437
YUBIN LEE <71171971+yubinnnn@users.noreply.github.com>
2022-05-10 18:39:00 +0900

    Add files via upload

---
+++ src/navigations/MainStack.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from 'styled-components/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { ChannelCreation } from '../screens';
+import MainTab from './MainTab';
+
+const Stack = createStackNavigator();
+
+const MainStack = () => {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerTintColor: theme.headerTintColor,
+        cardStyle: { backgroundColor: theme.background },
+        headerBackTitleVisible: false,
+      }}
+    >
+      <Stack.Screen name="<나와, 너>" component={MainTab} />
+      <Stack.Screen name="Channel Creation" component={ChannelCreation} />
+    </Stack.Navigator>
+  );
+};
+
+export default MainStack;
