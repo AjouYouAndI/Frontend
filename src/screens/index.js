d3b3fef4da706e727e90b9c59d29cb8d57815531
YUBIN LEE <71171971+yubinnnn@users.noreply.github.com>
2022-05-24 13:10:09 +0900

    Add files via upload

---
+++ src/screens/index.js
@@ -0,0 +1,9 @@
+import Login from './Login';
+import Signup from './Signup';
+import Channel from './Channel';
+import ChannelCreation from './ChannelCreation';
+import ChannelList from './ChannelList';
+import Profile from './Profile';
+import Rank from './Rank';
+
+export { Login, Signup, Channel, ChannelCreation, ChannelList, Profile, Rank };
