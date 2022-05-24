abecbb45ab0207f2763b57612d31853b6c15b881
YUBIN LEE <71171971+yubinnnn@users.noreply.github.com>
2022-05-24 13:11:32 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import App from './src/App';
+import React from 'react';
+import * as Location from 'expo-location';
+import { async } from '@firebase/util';
+import { Alert } from 'react-native-web';
+
+export default class extends React.Component {
+    geoLocation = async() => {
+        try{
+            const response = await Location.requestForegroundPermissionsAsync();
+            console.log(response);
+            const location = await Location.getCurrentPositionAsync();
+            console.log(location);
+            const {coords} = location;
+            console.log(coords.latitude, coords.longitude);
+        } catch(error) {
+            Alert.alert("위치를 찾을 수 없습니다.")
+        }
+    }
+
+    componentDidMount(){
+        this.geoLocation();
+    }
+    render(){
+        return <App />;
+    }
+}
