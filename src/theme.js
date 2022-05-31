7635a4447be0b13b82e9b08904ca1e7f453baa54
YUBIN LEE <71171971+yubinnnn@users.noreply.github.com>
2022-05-31 14:14:45 +0900

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -4,18 +4,21 @@ const colors = {
     grey_0: '#d5d5d5',
     grey_1: '#a6a6a6',
     red: '#e84118',
-    blue: '#3679fe',
-    pink: '#ffc0cb'
+    blue: '#384252',
+    pink: '#ee7764',
+    green: '#ccb996',
+    brown: '#5f8fc2',
+    brown1: '#8da5bd'
   };
 
 export const theme = {
     background: colors.white,
     text: colors.black,
-    errorText: colors.red,
-    label: colors.grey_1,
-    inputPlaceholder: colors.grey_1,
-    inputBorder: colors.grey_1,
-    inputDisabledBackground: colors.grey_0,
+    errorText: colors.grey_0,
+    label: colors.black,
+    inputPlaceholder: colors.black,
+    inputBorder: colors.black,
+    inputDisabledBackground: colors.white,
     pink: colors.pink,
 
     // Image Component
@@ -27,7 +30,7 @@ export const theme = {
     buttonBackground: colors.blue,
     buttonTitle: colors.white,
     buttonUnfilledTitle: colors.blue,
-    buttonLogout: colors.red,
+    buttonLogout: colors.grey_1,
 
     headerTintColor: colors.black,
     tabActiveColor: colors.blue,
@@ -36,8 +39,8 @@ export const theme = {
     spinnerBackground: colors.black,
     spinnerIndicator: colors.white,
 
-    listBorder: colors.grey_0,
-    listTime: colors.grey_1,
+    listBorder: colors.grey_1,
+    listTime: colors.white,
     listDescription: colors.grey_1,
-    listIcon: colors.black,
+    listIcon: colors.green,
 };
